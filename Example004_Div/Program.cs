3a8b8d8b0501ac895779770d5e1cdfb264d2c0cd
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-21 22:39:28 +0600

    Example004

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿/*int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+*/
+// ответ бует 2, а не 2,4 т.к. int целые числа.
+// для дробей используем double
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
