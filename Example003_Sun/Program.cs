82c68ba62cb71bc493b22726d7fd179caa7c9752
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-22 00:15:10 +0600

    Example005

--- Example003_Sun/Program.cs
+++ Example003_Sun/Program.cs
@@ -1,6 +1,7 @@
-﻿int numberA = 3; // int целое число, numberA присваивается - 3.
+﻿/*int numberA = 3; // int целое число, numberA присваивается - 3.
 int numberB = 5;
 Console.WriteLine(numberA+numberB);
+*/
 //или:
 /*
 int numberA = 3;
@@ -8,3 +9,10 @@ int numberB = 5;
 int result = numberA+numberB;
 Console.WriteLine(result);
 */
+//cумма случайных чисел:
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
+Console.WriteLine(result);
