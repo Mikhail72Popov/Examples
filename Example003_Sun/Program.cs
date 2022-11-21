122baaab19121b2c7be9588c99ea41995a5abf1f
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-21 22:27:00 +0600

    Examle003

---
+++ Example003_Sun/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = 3; // int целое число, numberA присваивается - 3.
+int numberB = 5;
+Console.WriteLine(numberA+numberB);
+//или:
+/*
+int numberA = 3;
+int numberB = 5;
+int result = numberA+numberB;
+Console.WriteLine(result);
+*/
