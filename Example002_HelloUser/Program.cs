75086cb2e3b50ba083c6a1f817034b55a807e6a6
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-21 22:03:08 +0600

    Example002

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+// в одну строку:
+/*Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+*/
