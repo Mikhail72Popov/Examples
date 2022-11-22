ffca7ec09c39e48e81316689ae078b440863f135
Mikhail <Popov.Mikhail.A@gmail.com>
2022-11-22 21:20:38 +0600

    Example009

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,23 @@
+﻿//нахождение макс. числа из массива
+//функция
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//массив
+//              0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+// соответствие числа порядковому номеру:
+//Console.WriteLine(array[5]);
+
+//вызов функции
+int max = Max(
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
